f41460ad6905aba2e86b9c12e61d5287e4c63a23
Jitterkkk <jitterzibetti@gmail.com>
2026-03-16 22:15:26 -0300

    Banco de dados / models

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Lead(Base):
+    __tablename__ = "leads"
+
+    id = Column(Integer, primary_key=True, index=True)
+    telefone = Column(String)
+    nome = Column(String)
+    interesse = Column(String)
+
+class Etapa(Base):
+    __tablename__ = "etapas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    telefone = Column(Integer, primary_key=True, index=true)
+    etapa = Column(String) 

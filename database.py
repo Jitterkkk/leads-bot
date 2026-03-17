a58eff423e3026489f920084d0ca1c4bb89ef925
Jitterkkk <jitterzibetti@gmail.com>
2026-03-17 10:58:55 -0300

    documentação database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# cria o banco SQlite (arquivo leads.db) e a sessão para interagir com ele
 engine = create_engine("sqlite:///leads.db")
 SessionLocal = sessionmaker(bind=engine)
-
+# base para a criação das tabelas (ORM)
 Base = declarative_base()

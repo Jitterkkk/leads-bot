7825fca61fa45d6be1f58db3642a963c73666b2d
Jitterkkk <jitterzibetti@gmail.com>
2026-03-17 10:59:15 -0300

    documentação models.py

--- models.py
+++ models.py
@@ -1,17 +1,19 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+# tabela de leads (clientes)
 class Lead(Base):
-    __tablename__ = "leads"
+    __tablename__ = "leads" # nome da tabela no banco de dados
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     telefone = Column(String)
     nome = Column(String)
     interesse = Column(String)
 
+# tabela para controlar a etapa do fluxo de cada usuário
 class Etapa(Base):
     __tablename__ = "etapas"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    telefone = Column(String, unique=True)
+    telefone = Column(String, unique=True) # cada telefone só pode ter uma etapa associada
     etapa = Column(String) 
